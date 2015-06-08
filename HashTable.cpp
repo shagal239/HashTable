5c9456189cbb103d85bac4c56c75aac5a378ff6a
OlegMishchuk <oleg_498@mail.ru>
2015-06-08 12:27:44 +0300

    First working version
    Initialization of pointers in the array.
    
    Passing parameters by reference;
    The array can be extended if there are collisions;
    Changed hash function.

--- HashTable.cpp
+++ HashTable.cpp
@@ -7,118 +7,131 @@
 //
 
 #include "HashTable.h"
-#include <iostream>
-#include <string>
-using namespace std;
-
-    unsigned long size;
-    HashList **hash_array;
-    
-unsigned long HashTable::HashFunction(const string str)
+	
+unsigned long HashTable::HashFunction(const string &str)
 {
-       return str.length() % size;
+	//Rs
+	static const unsigned long b = 378551;
+	unsigned long a = 63689;
+	unsigned long hash = 0;
+
+	for (unsigned int i = 0; i < str.length(); i++)
+	{
+		hash = hash * a + (unsigned char)str[i];
+		a *= b;
+	}
+
+	return hash % size;
 }
 
 void HashTable::ClearList(HashList *list)
 {
-    if (list->next) ClearList(list->next);
-    else free(list);
+	if (list->next) ClearList(list->next);
+	free(list);
+}
+
+void HashTable::RemapList(HashList *list)
+{
+	if (list->next) RemapList(list->next);
+	Add(list->key, list->value);
+	free(list);
+}
+
+void HashTable::ResizeArray(void)
+{
+	HashList **old_array = hash_array;
+	hash_array = new HashList*[size*=2];
+	for (unsigned long i = 0; i < size; i++) hash_array[i] = NULL;
+	for (unsigned long i = 0; i < size >> 1; i++)
+	{
+		if (old_array[i]) RemapList(old_array[i]);
+	}
+	free(old_array);
 }
 
 HashTable::HashTable()
 {
-    size = 256;
-    hash_array = new HashList*[size];
+	size = 256;
+	hash_array = new HashList*[size];
+	for (unsigned long i = 0; i < size; i++) hash_array[i] = NULL;
 }
-    
+	
 HashTable::HashTable(const unsigned long size)
 {
-    this->size = size;
-    hash_array = new HashList*[size];
+	this->size = size;
+	hash_array = new HashList*[size];
+	for (unsigned long i = 0; i < size; i++) hash_array[i] = NULL;
 }
 
 HashTable::~HashTable()
 {
-    for (unsigned long i = 0; i < size; i++)
-    {
-        if (hash_array[i]) ClearList(hash_array[i]);
-    }
+	for (unsigned long i = 0; i < size; i++)
+	{
+		if (hash_array[i]) ClearList(hash_array[i]);
+	}
+	free(hash_array);
 }
 
-void HashTable::Add(const string key, const string value)
+bool HashTable::Add(const string &key, const string &value)
 {
-    HashList *new_value = new HashList();
-    //new_value -> key.copy(key, key.length());
-    new_value -> key = key; //!!!
-    new_value -> value = value; //!!!
-    new_value -> next = NULL;
-    if(hash_array[HashFunction(key)])
-    {
-        hash_array[HashFunction(key)] = new_value;
-        return;
-    }
-    HashList *last_value = hash_array[HashFunction(key)];
-    if (last_value)
-    {
-        for (;;)
-        {
-            if (!last_value->key.compare(key))
-            {
-                //exceprion
-                free(new_value);
-                return;
-            }
-            if (last_value->next) last_value = last_value->next;
-            else break;
-        }
-        last_value->next = new_value;
-    }
-    else
-    {
-        hash_array[HashFunction(key)] = new_value;
-    }
+	HashList *new_value = new HashList();
+	new_value -> key = key;
+	new_value -> value = value;
+	new_value -> next = NULL;
+	HashList *last_value;
+	if ((last_value = hash_array[HashFunction(key)]))
+	{
+		int count = 1;
+		for (;;++count)
+		{
+			if (!last_value->key.compare(key))
+			{
+				free(new_value);
+				return false;
+			}
+			if (last_value->next) last_value = last_value->next;
+			else break;
+		}
+		last_value->next = new_value;
+		if (count > 2) ResizeArray();
+	}
+	else hash_array[HashFunction(key)] = new_value;
+	return true;
 }
 
-string HashTable::Search(const string key)
+string* HashTable::Search(const string &key)
 {
-    HashList *current_value = hash_array[HashFunction(key)];
-    if (current_value)
-        for (;;)
-        {
-           if (!current_value->key.compare(key)) return current_value->value;
-           if (current_value->next) current_value = current_value->next;
-           else break;
-        }
-        return string();
+	HashList *current_value = hash_array[HashFunction(key)];
+	if (current_value)
+		for (;;)
+		{
+		   if (!current_value->key.compare(key)) return &current_value->value;
+		   if (current_value->next) current_value = current_value->next;
+		   else break;
+		}
+		return NULL;
 }
-    
-void HashTable::Delete(const string key)
+	
+bool HashTable::Delete(const string &key)
 {
-    HashList *current_value = hash_array[HashFunction(key)];
-    HashList *last_value = current_value;
-    if (current_value)
-        for (;;)
-        {
-            if (!current_value->key.compare(key))
-            {
-                if (last_value == current_value)
-                {
-                    hash_array[HashFunction(key)] = current_value->next;
-                    free(current_value);
-                }
-                else
-                {
-                    last_value = current_value->next;
-                    free(current_value);
-                }
-                return;
-            }
-            if (current_value->next)
-            {
-                last_value = current_value;
-                current_value = current_value->next;
-            }
-            else break;
-        }
-    //exception
+	HashList *current_value = hash_array[HashFunction(key)];
+	HashList *last_value = current_value;
+	if (current_value)
+		for (;;)
+		{
+			if (!current_value->key.compare(key))
+			{
+				if (last_value == current_value) hash_array[HashFunction(key)] = current_value->next;
+				else last_value = current_value->next;
+				free(current_value);
+				return true;
+			}
+			if (current_value->next)
+			{
+				last_value = current_value;
+				current_value = current_value->next;
+			}
+			else break;
+		}
+	return false;
 }
--- HashTable.h
+++ HashTable.h
@@ -8,37 +8,33 @@
 
 #ifndef test_HashTable_h
 #define test_HashTable_h
-#include <iostream>
 #include <string>
 using namespace std;
 
 struct HashList
 {
-    string key, value;
-    struct HashList *next;
+	string key, value;
+	struct HashList *next;
 };
 
 class HashTable
 {
 private:
-    unsigned long size;
-    HashList **hash_array;
-    
-    unsigned long HashFunction(const string str);
-    void ClearList(HashList *list);
-    
+	unsigned long size;
+	HashList **hash_array;
+	
+	unsigned long HashFunction(const string &str);
+	void ClearList(HashList *list);
+	void RemapList(HashList *list);
+	void ResizeArray(void);
+	
 public:
-    HashTable();
-    
-    HashTable(const unsigned long size);
-    
-    ~HashTable();
-    
-    void Add(const string key, const string value);
-    
-    string Search(const string key);
-    
-    void Delete(const string key);
+	HashTable();
+	HashTable(const unsigned long size);
+	~HashTable();
+	bool Add(const string &key, const string &value);
+	string* Search(const string &key);
+	bool Delete(const string &key);
 };
 
 #endif
